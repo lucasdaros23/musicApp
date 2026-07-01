48b5dd37f40b6e347067fe73c37e9e653d34bafe
user <lucasdarosparente@gmail.com>
2026-07-01 10:42:30 -0300

    changes

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.musicapp.feature.edit.EditScreen
 import com.example.musicapp.feature.main.MainScreen
 import com.example.musicapp.feature.main.MainViewModel
 import com.example.musicapp.ui.theme.MusicAppTheme
@@ -23,9 +24,12 @@ class MainActivity : ComponentActivity() {
             MusicAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val mainViewModel: MainViewModel = hiltViewModel()
-                    MainScreen(
+//                    MainScreen(
+//                        modifier = Modifier.padding(innerPadding),
+//                        viewModel = mainViewModel
+//                    )
+                    EditScreen(
                         modifier = Modifier.padding(innerPadding),
-                        viewModel = mainViewModel
                     )
                 }
             }
--- app/src/main/java/com/example/musicapp/feature/edit/EditScreen.kt
+++ app/src/main/java/com/example/musicapp/feature/edit/EditScreen.kt
@@ -10,15 +10,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.musicapp.R
+import com.example.musicapp.feature.components.IconComponent
 import com.example.musicapp.feature.components.TileIcon
 import com.example.musicapp.ui.theme.Size
 import com.example.musicapp.ui.theme.blue
@@ -30,37 +35,57 @@ import com.example.musicapp.ui.theme.greenLight
 import com.example.musicapp.ui.theme.orange
 import com.example.musicapp.ui.theme.orangeBorder
 import com.example.musicapp.ui.theme.orangeLight
+import com.example.musicapp.ui.theme.pink
+import com.example.musicapp.ui.theme.pinkBorder
+import com.example.musicapp.ui.theme.pinkLight
 import com.example.musicapp.ui.theme.uiBlueLight
 import com.example.musicapp.ui.theme.yellow
 import com.example.musicapp.ui.theme.yellowBorder
 import com.example.musicapp.ui.theme.yellowLight
 
 @Composable
-fun EditScreen() {
+fun EditScreen(modifier: Modifier) {
     Column(
-        Modifier
+        modifier
             .fillMaxHeight()
             .padding(Size.mini)
     ) {
         Row(
             Modifier.fillMaxWidth()
+                .padding(vertical = 10.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = "x",
+            IconComponent(
+                id = R.drawable.close_icon,
                 color = uiBlueLight,
-                fontSize = 40.sp
+                "",
+                modifier= Modifier.size(Size.medium)
+            )
+            Spacer(Modifier.width(Size.regular))
+            Text(
+                "Escolha o Instrumento",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White.copy(alpha = .9f)
             )
         }
-        ListItem(Triple(orange, orangeLight, orangeBorder))
-        ListItem(Triple(blue, blueLight, blueBorder))
-        ListItem(Triple(yellow, yellowLight, yellowBorder))
-        ListItem(Triple(green, greenLight, greenBorder))
+
+        LazyColumn() {
+            item {
+                ListItem(Triple(orange, orangeLight, orangeBorder), text = "Caixa")
+                ListItem(Triple(blue, blueLight, blueBorder),"Prato")
+                ListItem(Triple(yellow, yellowLight, yellowBorder), "Bumbo")
+                ListItem(Triple(green, greenLight, greenBorder), "Triângulo")
+                ListItem(Triple(pink, pinkLight, pinkBorder), "Conga")
+            }
+        }
     }
 }
 
 @Composable
 private fun ListItem(
-    colors: Triple<Color, Color, Color>
+    colors: Triple<Color, Color, Color>,
+    text: String,
 ) {
     val shape = RoundedCornerShape(size = Size.tiny)
     Column {
@@ -79,10 +104,17 @@ private fun ListItem(
         ) {
             Spacer(Modifier.size(Size.tiny))
             TileIcon(
-                id = R.drawable.drum_icon,
+                id = R.drawable.play_icon,
                 backgroundColor = colors.second,
                 iconColor = colors.first,
                 size = Size.regular
+            )
+            Spacer(Modifier.size(Size.tiny))
+            Text(
+                text,
+                fontSize = 20.sp,
+                color = Color.White.copy(alpha = .9f),
+                fontWeight = FontWeight.Bold
             )
         }
         Spacer(Modifier.size(Size.mini))
@@ -92,5 +124,5 @@ private fun ListItem(
 @Preview(showBackground = true, device = "spec:width=640dp,height=360dp,orientation=landscape")
 @Composable
 private fun EditScreenPreview() {
-    EditScreen()
+    EditScreen(Modifier)
 }
